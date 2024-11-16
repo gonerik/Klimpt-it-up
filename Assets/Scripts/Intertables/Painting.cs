dd544fc252e01094cacaa4b2abd6d4113e71e657
gonerik <163130399+gonerik@users.noreply.github.com>
2024-11-16 18:03:12 +0100

    NExt level fix

--- Assets/Scripts/Intertables/Painting.cs
+++ Assets/Scripts/Intertables/Painting.cs
@@ -3,6 +3,11 @@ using Intertables;
 
 public class Painting : PickUpObjects
 {
+    private void Start()
+    {
+        Storage.addPainting();
+    }
+
     public override void Interact()
     {
         base.Interact();
--- Assets/Scripts/Intertables/Storage.cs
+++ Assets/Scripts/Intertables/Storage.cs
@@ -8,12 +8,13 @@ using UnityEngine.SceneManagement;
 public class Storage : Interactable
 {
     private static int levelCount = 1;
-    private int paintingsCount = 0;
+    private static int paintingsCount = 0;
     private int paintingsCollected = 0;
-    public void Start()
+    public void Awake()
     {
         paintingsCount = 0;
         levelCount++;
+        
     }
 
     public override void Interact()
@@ -21,19 +22,24 @@ public class Storage : Interactable
         base.Interact();
         paintingsCollected++;
         CharacterController2D.Instance.SetSpeed(CharacterController2D.Instance.maxRunSpeed);
+        Debug.Log(paintingsCollected);
+        Debug.Log(paintingsCount);
+        
         if (paintingsCollected == paintingsCount)
         {
-            
+            StartCoroutine(GoToNextLevel());
         }
     }
 
-    public void addPainting()
+    public static void addPainting()
     {
         paintingsCount++;
+        Debug.Log(paintingsCount);
     }
 
     public IEnumerator GoToNextLevel(){
         //Play win animation here
+        Debug.Log(paintingsCollected);
         CharacterController2D.Instance.setPlayerMovement(false);
         yield return new WaitForSeconds(5);
         SceneManager.LoadSceneAsync(levelCount);
