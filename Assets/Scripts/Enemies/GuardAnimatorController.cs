8e6ad0c33d40fdf3cfe59d2ce62a42fbaa284a51
s28398 <64951198+SystemCall213@users.noreply.github.com>
2024-11-16 21:31:30 +0100

    guard walk logic done

--- Assets/Scripts/Enemies/GuardAnimatorController.cs
+++ Assets/Scripts/Enemies/GuardAnimatorController.cs
@@ -13,6 +13,20 @@ public class GuardAnimatorController : MonoBehaviour
 
     void Update()
     {
-        animator.Play("Guard_walk_back");
+        GameObject currentGuardWaypoint = pathFollower.waypoints[pathFollower.GetCurrentWaypointIndex()];
+        float XAbs = Mathf.Abs(currentGuardWaypoint.transform.position.x - transform.position.x);
+        if (XAbs > 0.1) {
+            if (currentGuardWaypoint.transform.position.x > transform.position.x) {
+                animator.Play("Guard_walt_right");
+            } else {
+                animator.Play("Guard_walk_left");
+            }
+        } else {
+            if (currentGuardWaypoint.transform.position.y > transform.position.y) {
+                animator.Play("Guard_walk_back");
+            } else {
+                animator.Play("Guard_walk_front");
+            }
+        }
     }
 }
